54005d58037e623a3f0d1c70cfc2d724e1865d68
Kilian Mayer <mayer-kilian@gmx.de>
2026-08-09 09:07:40 +0200

    improved error handling in main.ts

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -1,4 +1,9 @@
 import "./api/server.ts";
 import { runWorkflow } from "./runtime/workflow.ts";
 
-Deno.exit(await runWorkflow());
+try {
+  Deno.exit(await runWorkflow());
+} catch (error) {
+  console.error(error);
+  Deno.exit(1);
+}
