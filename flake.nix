3f8aaf2727ed8b4f2dc90412c52eb6c2ab581336
Kilian Mayer <mayer-kilian@gmx.de>
2026-08-25 12:03:50 +0200

    chore: add reproducible Nix development environment

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      allSystems = [
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs allSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              bun
+              bubblewrap
+              agent-browser
+              chromium
+              python3
+            ];
+
+            shellHook = ''
+              export AGENT_BROWSER_EXECUTABLE_PATH="${pkgs.chromium}/bin/chromium"
+            '';
+          };
+        }
+      );
+    };
+}
